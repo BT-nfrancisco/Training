6017e4e294e4b8b5f6c7c659710a01408f5d523b
Nadal Francisco Garcia <nadal.francisco@braintec-group.com>
2018-05-08 11:54:57 +0200

    Exercise - Define basic views for Course

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': "Openacademy",
+    'name': "openacademy",
     'version': '1.0',
     'depends': ['base'],
     'author': "Nadal",
@@ -9,11 +9,12 @@
                    """,
     # data files always loaded at installation
     'data': [
-        "views/courses_menu.xml"
+        "views/courses_menu.xml",
+        "views/course_views.xml"
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
         "views/demo_data.xml"
     ],
-    'auto_install': False # If module and all its dependencies have to be installed automatically
+    'auto_install': False  # If module and all its dependencies have to be installed automatically
 }
--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -3,9 +3,10 @@
 
 from odoo import fields, models
 
+
 class Course(models.Model):
     _name = 'course'
     _rec_name = 'title'
 
     title = fields.Char(string='Title')
-    description = fields.Text(string='Description of the subject')
+    description = fields.Text(string='Description of the course')
