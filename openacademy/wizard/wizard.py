d230f27591b26494a633723472876a0180e8f3f5
Nadal Francisco Garcia <nadal.francisco@braintec-group.com>
2018-05-10 11:31:35 +0200

    Exercise - wizard completed first part

--- openacademy/wizard/wizard.py
+++ openacademy/wizard/wizard.py
@@ -5,5 +5,17 @@ class Wizard(models.TransientModel):
     _name = 'wizard'
     _rec_name = 'wizard_session'
 
+    def get_default_session(self):
+        return self._context.get('active_id')
+
+    # wizard_session = fields.Many2one('session', string='Session', default=get_default_session)
     wizard_session = fields.Many2one('session', string='Session')
     attendees = fields.Many2many('res.partner', 'partners_ref', string="Attendees")
+
+    @api.one
+    def save_results(self):
+        for new_attendee in self.attendees:
+            self.wizard_session.attendees.write({(4, new_attendee.id)})
+
+    def save_results_v11(self):
+        self.session.attendees |= self.attendees
