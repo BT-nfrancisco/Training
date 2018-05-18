12b4d14f543ef099e92f490de44898a8cbc0d36e
Nadal Francisco Garcia <nadal.francisco@braintec-group.com>
2018-05-18 17:05:48 +0200

    New wizard

--- openacademy/wizard/add_sessions_wizard.py
+++ openacademy/wizard/add_sessions_wizard.py
@@ -8,13 +8,15 @@ class AddSessionsWizard(models.TransientModel):
     def get_default_session(self):
         return self._context.get('active_id')
 
-    # wizard_session = fields.Many2one('session', string='Session')
+    wizard_session = fields.Many2one('session', string='Session')
+    wizard_session_rel = fields.Many2many('session', 'wizard_session_ref',
+                                          string='Sessions')
     wizard_session_attendees = fields.Many2many('res.partner',
                                                 'wizard_partners_ref',
                                                 string="Attendees")
 
     @api.multi
-    def save_results(self):
+    def save_results_v10(self):
         attendees_id = []
         for new_attendee in self.wizard_session_attendees:
             attendees_id.append(new_attendee.id)
@@ -22,6 +24,12 @@ class AddSessionsWizard(models.TransientModel):
         self.wizard_session.attendees.write({(6, 0, attendees_id)})
 
     @api.multi
-    def save_results_11(self):
-        for new_attendee in self.wizard_session_attendees:
-            self.wizard_session.attendees |= new_attendee
+    def save_results(self):
+
+        session_ids = self._context.get('active_ids')
+        selected_sessions = self.env['session'].search(
+            [('id', 'in', session_ids)])
+
+        for session in selected_sessions:
+            for new_attendee in self.wizard_session_attendees:
+                session.attendees |= new_attendee
